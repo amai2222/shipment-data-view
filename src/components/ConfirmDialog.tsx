8abf2eb07eb06f9ac62897f86b8dbcd599ddb9e7
amai2222 <dmuxue2@gmail.com>
2025-07-25 10:09:37 +0800

    Create ConfirmDialog.tsx

---
+++ src/components/ConfirmDialog.tsx
@@ -0,0 +1,42 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import React from "react";
+
+interface ConfirmDialogProps {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  onConfirm: () => void;
+}
+
+export function ConfirmDialog({
+  children,
+  title,
+  description,
+  onConfirm,
+}: ConfirmDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>取消</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>确认</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
