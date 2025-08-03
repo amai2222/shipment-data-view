301caa984b9db1fecb6ac3c5b60dc51269574ea9
amai2222 <dmuxue2@gmail.com>
2025-08-03 10:37:11 +0800

    Update LogisticsFormDialog.tsx

--- src/pages/BusinessEntry/components/LogisticsFormDialog.tsx
+++ src/pages/BusinessEntry/components/LogisticsFormDialog.tsx
@@ -7,7 +7,7 @@ import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Loader2 } from "lucide-react";
-import { AsyncCreatableCombobox, Option } from './AsyncCreatableCombobox'; // [核心修复] - 修正了导入路径
+import { AsyncCreatableCombobox, Option } from './AsyncCreatableCombobox';
 import { LogisticsFormData, Project, PartnerChain, Driver } from '../types';
 
 interface LogisticsFormDialogProps {
@@ -48,7 +48,8 @@ export function LogisticsFormDialog({ isOpen, onOpenChange, onSubmit, isSubmitti
           
           <div className="col-span-1 space-y-1">
             <Label>司机 *</Label>
-            <AsyncCreatableCombobox value={formData.driver_name} onValueChange={handleDriverChange} tableName="drivers" searchColumn="name" placeholder="选择或创建司机" searchPlaceholder="搜索司机..." disabled={!formData.project_id || isSubmitting} />
+            {/* [核心重写] - 传递 projectId */}
+            <AsyncCreatableCombobox value={formData.driver_name} onValueChange={handleDriverChange} tableName="drivers" searchColumn="name" placeholder="选择或创建司机" searchPlaceholder="搜索司机..." projectId={formData.project_id} disabled={isSubmitting} />
           </div>
           <div className="col-span-1 space-y-1"><Label>车牌号</Label><Input value={formData.license_plate || ''} onChange={(e) => handleInputChange('license_plate', e.target.value)} disabled={isSubmitting} /></div>
           <div className="col-span-1 space-y-1"><Label>司机电话</Label><Input value={formData.driver_phone || ''} onChange={(e) => handleInputChange('driver_phone', e.target.value)} disabled={isSubmitting} /></div>
@@ -56,13 +57,15 @@ export function LogisticsFormDialog({ isOpen, onOpenChange, onSubmit, isSubmitti
           
           <div className="col-span-1 space-y-1">
             <Label>装货地点 *</Label>
-            <AsyncCreatableCombobox value={formData.loading_location} onValueChange={(_, rawValue) => handleInputChange('loading_location', rawValue)} tableName="locations" searchColumn="name" placeholder="选择或创建地点" searchPlaceholder="搜索地点..." disabled={!formData.project_id || isSubmitting} />
+            {/* [核心重写] - 传递 projectId */}
+            <AsyncCreatableCombobox value={formData.loading_location} onValueChange={(_, rawValue) => handleInputChange('loading_location', rawValue)} tableName="locations" searchColumn="name" placeholder="选择或创建地点" searchPlaceholder="搜索地点..." projectId={formData.project_id} disabled={isSubmitting} />
           </div>
           <div className="col-span-1 space-y-1"><Label>装货重量</Label><Input type="number" placeholder="吨" value={formData.loading_weight || ''} onChange={(e) => handleInputChange('loading_weight', e.target.value)} disabled={isSubmitting} /></div>
           
           <div className="col-span-1 space-y-1">
             <Label>卸货地点 *</Label>
-            <AsyncCreatableCombobox value={formData.unloading_location} onValueChange={(_, rawValue) => handleInputChange('unloading_location', rawValue)} tableName="locations" searchColumn="name" placeholder="选择或创建地点" searchPlaceholder="搜索地点..." disabled={!formData.project_id || isSubmitting} />
+            {/* [核心重写] - 传递 projectId */}
+            <AsyncCreatableCombobox value={formData.unloading_location} onValueChange={(_, rawValue) => handleInputChange('unloading_location', rawValue)} tableName="locations" searchColumn="name" placeholder="选择或创建地点" searchPlaceholder="搜索地点..." projectId={formData.project_id} disabled={isSubmitting} />
           </div>
           <div className="col-span-1 space-y-1"><Label>卸货重量</Label><Input type="number" placeholder="吨" value={formData.unloading_weight || ''} onChange={(e) => handleInputChange('unloading_weight', e.target.value)} disabled={isSubmitting} /></div>
           <div className="col-span-1 space-y-1"><Label>运费金额 (元)</Label><Input type="number" value={formData.current_cost || ''} onChange={(e) => handleInputChange('current_cost', e.target.value)} disabled={isSubmitting} /></div>
